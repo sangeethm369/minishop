5fb228124966c648ebdfbbd9eadb8d93b86d52d9
sangeeth <sangeethm369@gmail.com>
2024-09-04 21:25:23 +0530

    mini

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-// import 'package:minishop/home_pg.dart';
-// import 'package:minishop/profile.dart';
+import 'package:lottie/lottie.dart';
 import 'package:minishop/login_pg.dart';
-// import 'package:minishop/signup_pg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +12,37 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-     home: LoginPg(),
+     home: IntroPg(),
+    );
+  }
+}
+
+
+class IntroPg extends StatefulWidget {
+  const IntroPg({super.key});
+
+  @override
+  State<IntroPg> createState() => _IntroPgState();
+}
+
+class _IntroPgState extends State<IntroPg> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple.shade100,
+      body: Center(
+        child: Lottie.asset("assets/images/lottiecart.json"),
+      ),
+      floatingActionButton:  Container(width: 100,
+        child: FloatingActionButton(
+          onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPg(),));
+          },shape:  RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          tooltip: "Continue to Login page",
+          splashColor: Colors.indigoAccent,
+          child: const Icon(Icons.arrow_forward_sharp),
+          ),
+      ),
     );
   }
 }
